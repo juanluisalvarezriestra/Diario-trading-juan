e79338280d60f7dd42c6c6414a0b6d8538d3a925
juanluisalvarezriestra <riestracovian@gmail.com>
2026-03-15 21:27:00 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,1 @@
+const CACHE='diario-trading-pro-v8';const URLS=['./','./index.html','./manifest.json','./service-worker.js','./icon-192.png','./icon-512.png'];self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(URLS)))});self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
